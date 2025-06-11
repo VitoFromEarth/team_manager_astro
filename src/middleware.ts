ce1a192467aedb75b23171c48bc2700cb49af1b4
Vitalii Kononenko <vitofromearth@gmail.com>
2025-06-11 18:21:12 +0300

    Updated pages for Sign In and Sign Up. Updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { sequence } from "astro:middleware";
 import type { MiddlewareHandler } from "astro";
 
 async function checkAuth(context: Parameters<MiddlewareHandler>[0], next: Parameters<MiddlewareHandler>[1]) {
-	const publicRoutes = ["/signin", "/register", "/api/auth/signin", "/api/auth/callback"];
+	const publicRoutes = ["/signin", "/register", "/api/auth/register" ,"/api/auth/signin", "/api/auth/callback"];
 	const accessToken = context.cookies.get("sb-access-token");
 	const refreshToken = context.cookies.get("sb-refresh-token");
 
